03800059fa4de81fa0c8079d1856b7b39171f771
Jaiswal Yasaswi <yasaswijaiswal@gmail.com>
2026-06-06 02:35:30 +0530

    Add: binary-search - submission-8

---
+++ Data Structures & Algorithms/binary-search/submission-8.py
@@ -0,0 +1,18 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+        left = 0
+        right = len(nums)-1
+        
+        while left <= right:
+            n = (left + right)//2
+            if(nums[n] == target):
+                return n
+            elif target > nums[n]:
+                left = n +1            
+            else:
+                right = n-1
+        return -1
+
+
+        
